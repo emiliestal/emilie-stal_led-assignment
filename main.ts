a9d8680903cce914f08aad33c79d46375c28765f
Emilie <68868315+emiliestal@users.noreply.github.com>
2020-08-11 23:23:54 -0400

    Final project - complete

--- main.ts
+++ main.ts
@@ -1,13 +1,55 @@
-function LightUp (num: number) {
-    led.plot(num % 5, num / 5)
-}
 input.onButtonPressed(Button.A, function () {
-    Position += 1
+    selected = randint(0, OffLights.length - 1)
+    selected = OffLights.removeAt(selected)
+    led.plot(selected % 5, selected / 5)
+    OnLights.push(selected)
+    if (OffLights.length == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showString("RESET")
 })
 input.onButtonPressed(Button.B, function () {
-    Position += -1
+    selected = OnLights.pop()
+    led.unplot(selected % 5, selected / 5)
+    OffLights.push(selected)
+    if (OnLights.length == 0) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
-let Position = 7
-basic.forever(function () {
-    LightUp(Position)
-})
+let selected = 0
+let OnLights: number[] = []
+let OffLights: number[] = []
+OffLights = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+OnLights = []
+let OnOff = true
